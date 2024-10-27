40b7a9c2475fc9a2b84a7f3dc83f68d5f88cb22f
abdulmateenGit <abdulmateen.lhr00@gmail.com>
2024-10-27 09:24:51 +0500

    Product Router & Controllers

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,18 +1,18 @@
 import { Router } from "express";
-
+import {
+  listProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "./productsController";
 
 const router = Router();
 
-
-router.get("/", (req, res) => {
-  res.send("List of products");
-});
-router.post("/", (req, res) => {
-  res.send("New Product Created");
-});
-router.post("/:id", (req, res) => {
-  console.log(req.params);
-  res.send("Product by id");
-});
+router.get("/", listProducts);
+router.post("/", createProduct);
+router.get("/:id", getProductById);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
